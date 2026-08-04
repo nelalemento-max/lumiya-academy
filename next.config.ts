ff78bc94278a0851b761278b39ea42cb3c177bea
nelalemento-max <nelalemento@gmail.com>
2026-08-04 11:48:57 -0400

    Prepare Lumiya Academy for Firebase Hosting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  typescript: {
+    tsconfigPath: "tsconfig.firebase.json",
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
